4f26971d720d35291e27958d3cc2b2f03e73943c
John Bryan Valle <jbvalle.jv@gmail.com>
2024-04-05 16:44:02 +0200

    cleaned

--- target/thumbv7em-none-eabihf/debug/deps/kyrax_stm32f401re-a836e6b69875a15d.d
+++
@@ -1,8 +0,0 @@
-/home/kyrax/personal/coding/035_kyrax_embedded/034_kyrax_embedded/target/thumbv7em-none-eabihf/debug/deps/libkyrax_stm32f401re-a836e6b69875a15d.rmeta: src/lib.rs src/peripherals/mod.rs src/peripherals/gpiox.rs src/peripherals/rcc.rs
-
-/home/kyrax/personal/coding/035_kyrax_embedded/034_kyrax_embedded/target/thumbv7em-none-eabihf/debug/deps/kyrax_stm32f401re-a836e6b69875a15d.d: src/lib.rs src/peripherals/mod.rs src/peripherals/gpiox.rs src/peripherals/rcc.rs
-
-src/lib.rs:
-src/peripherals/mod.rs:
-src/peripherals/gpiox.rs:
-src/peripherals/rcc.rs:
